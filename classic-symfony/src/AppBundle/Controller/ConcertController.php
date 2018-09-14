4f05ecc7ff44eab66c146944077e08708828daf2
椿山 <tsubakiyama@itounoMacBook-puro.local>
2018-09-14 12:10:43 +0900

    第４章終わり

--- classic-symfony/src/AppBundle/Controller/ConcertController.php
+++ classic-symfony/src/AppBundle/Controller/ConcertController.php
@@ -10,7 +10,7 @@ class ConcertController extends Controller
     /**
      * @Route("/concert/")
      */
-    public function indexAction()
+        public function indexAction()
     {
         $concertList = [
             [
---
+++ classic-symfony/src/AppBundle/Controller/InquiryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/inquiry")
+ */
+class InquiryController extends Controller
+{
+    private function createInquiryForm()
+    {
+        return $this->createFormBuilder()
+            ->add('name', 'text')
+            ->add('email', 'text')
+            ->add('tel', 'text', [
+                'required' => false,
+            ])
+            ->add('type', 'choice', [
+                'choices' => [
+                    '公演について',
+                    'その他',
+                ],
+                'expanded' => true,
+            ])
+            ->add('content', 'textarea')
+            ->add('submit', 'submit', [
+                'label' => '送信',
+            ])
+            ->getForm();
+    }
+
+    /**
+     * @Route("/")
+     * @Method("get")
+     */
+    public function indexAction()
+    {
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $this->createInquiryForm()->createView()]
+        );
+    }
+
+    /**
+     * @Route("/complete")
+     */
+    public function completeAction()
+    {
+        return $this->render('Inquiry/complete.html.twig');
+    }
+
+    /**
+     * @Route("/")
+     * @Method("post")
+     */
+    public function indexPostAction(Request $request)
+    {
+        $form = $this->createInquiryForm();
+        $form->handleRequest($request);
+        if ($form->isValid())
+        {
+            $data = $form->getData();
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Webサイトからのお問い合わせ')
+                ->setFrom('webmaster@example.com')
+                ->setTo('admin@example.com')
+                ->setBody(
+                    $this->renderView(
+                        'mail/inquiry.txt.twig',
+                        ['data' => $data]
+                    )
+                );
+
+            $this->get('mailer')->send($message);
+
+            return $this->redirect(
+                $this->generateUrl('app_inquiry_complete'));
+        }
+
+        return $this->render('Inquiry/index.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+}
